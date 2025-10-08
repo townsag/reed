ca65916beb44d36c38f0e425f413e30a394314c6
Andrew Townsend <townsag@gmail.com>
2025-10-07 21:44:07 -0700

    feat: added entrypoint for the user server

---
+++ user_service/cmd/server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/townsag/reed/user_service/internal/config"
+	"github.com/townsag/reed/user_service/internal/repository"
+	"github.com/townsag/reed/user_service/internal/service"
+	"github.com/townsag/reed/user_service/internal/server"
+	pb "github.com/townsag/reed/user_service/api"
+)
+
+
+func main() {
+	// create a connection to the database
+	cfg, err := config.GetConfiguration()
+	if err != nil {
+		log.Fatalf("failed to get database connection configuration: %v", err)
+	}
+	pool, err := config.CreateDBConnectionPool(context.Background(), cfg)
+	if err != nil {
+		log.Fatalf("failed to create database connection pool: %v", err)
+	}
+	defer pool.Close()
+	// create a repo
+	userRepo := repository.NewUserRepository(pool)
+	// create a service
+	userService := service.NewUserService(userRepo)
+	// create a server
+	userServer := server.NewUserServiceImpl(userService)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, userServer)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ user_service/internal/config/postgres.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"strconv"
+	"fmt"
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	// "github.com/exaring/otelpgx"
+	// TODO: ^add otel tracing to the pgxpool
+
+	"github.com/townsag/reed/user_service/internal/util"
+)
+
+func GetConfiguration() (*pgxpool.Config, error) {
+	var portEnv string = util.GetEnvWithDefault("POSTGRES_PORT", "5432")
+	port, err := strconv.Atoi(portEnv)
+	if err != nil {
+		port = 5432
+	}
+	host := util.GetEnvWithDefault("POSTGRES_HOST", "localhost")
+	dbName := util.GetEnvWithDefault("POSTGRES_DB", "postgres")
+	user := util.GetEnvWithDefault("POSTGRES_USER", "admin")
+	password := util.GetEnvWithDefault("POSTGRES_PASSWORD", "password")
+	poolMaxCons := util.GetEnvWithDefault("POOL_MAX_CONS", "25")
+
+	cfg, err := pgxpool.ParseConfig(fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s pool_max_conns=%s",
+		host, port, user, password, dbName, poolMaxCons,
+	))
+	if err != nil {
+		return nil, err
+	}
+	// cfg.ConnConfig.Tracer = otelpgx.NewTracer()
+	return cfg, nil	
+}
+
+func CreateDBConnectionPool(ctx context.Context, config *pgxpool.Config) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a database connection pool: %w", err)
+	}
+	// if err = otelpgx.RecordStats(pool, otelpgx.WithMinimumReadDBStatsInterval(time.Second * 1)); err != nil {
+	// 	pool.Close()
+	// 	return nil, fmt.Errorf("failed to set up database connection pool observability: %w", err)
+	// }
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("failed to ping the new connection pool: %w", err)
+	}
+	return pool, nil
+}

7089e0a2b9b14ca488d9144cf1d87913557905b6
Andrew Townsend <townsag@gmail.com>
2025-10-17 20:00:42 -0700

    feat: add document service repository package

---
+++ document_service/internal/service/document_service.go
@@ -0,0 +1,114 @@
+package service
+
+import (
+	"context"
+	"time"
+)
+
+// TODO: come up with a definitive approach for creating enums
+//		 in the mean time, use integer values of named types in memory
+//		 but use strings in the database and when returning to the caller.
+//		 this balances the simplicity of using integers with the readability
+//		 of using strings
+type Permission int32
+const (
+	Viewer Permission = iota
+	Editor 
+	Owner
+)
+
+type SortField int32
+const (
+	CreatedAt SortField = iota
+	LastModifiedAt
+)
+
+type Document struct {
+	ID string
+	Name *string
+	Description *string
+	CreatedAt time.Time
+	LastModifiedAt time.Time
+}
+
+type Cursor struct {
+	SortField SortField
+	LastSeenTime time.Time
+	LastSeenDocument string
+}
+
+type DocumentPermission struct {
+	Document Document
+	Permission Permission
+}
+
+type RecipientPermission struct {
+	RecipientId string
+	Permission Permission
+	CreatedAt time.Time
+	LastModifiedAt time.Time
+}
+
+/*
+Open questions:
+- should the calling code or the repository be in charge of generating
+  UUIDs?
+	- leave the generation of UUIDs to the repository so that the repository
+	  can optionally choose implementation specific uuids like uuid7
+	- if we ever want to migrate to a system with more explicit generation of
+	  ids or a central id provider then generating ids at the service level
+	  will be preferable
+*/
+
+type DocumentRepository interface {
+	CreateDocument(ctx context.Context, userId int32, documentName *string, documentDescription *string) (documentId string, err error)
+	GetDocument(ctx context.Context, documentId string) (document *Document, err error)
+	UpdateDocument(ctx context.Context, documentId string, documentName *string, documentDescription *string) (err error)
+	DeleteDocument(ctx context.Context, documentId string) (err error)
+	// list the documents that are associated with that user at those permission levels
+	ListDocumentsByPrincipal(ctx context.Context, principalId string, permissions []Permission, cursor *Cursor, pageSize int32) (documentPermissions []DocumentPermission, cursorResp *Cursor, err error)
+	GetPermissionOfPrincipalOnDocument(ctx context.Context, documentId string, principalId string) (permission Permission, err error)
+	ListPermissionsOnDocument(ctx context.Context, documentId string) (recipientPermissions []RecipientPermission, err error)
+	CreateGuest(ctx context.Context, creatorId string, documentId string, permission Permission) (guestId string, err error)
+	UpsertPermissionsUser(ctx context.Context, userId string, documentId string, permission Permission) (err error)
+	UpdatePermissionGuest(ctx context.Context, guestId string, documentId string, permission Permission) (err error)
+	DeletePermissionsPrincipal(ctx context.Context, recipientId string, documentId string) (err error)
+}
+
+type DocumentService struct {
+	documentRepo DocumentRepository
+}
+
+func NewDocumentService(documentRepo DocumentRepository) *DocumentService {
+	return &DocumentService{
+		documentRepo: documentRepo,
+	}
+}
+
+func (ds *DocumentService) CreateDocument(
+	ctx context.Context,
+	ownerUserId int32,
+	documentName *string,
+	documentDescription *string,
+) (string, error) {
+	// this is an internal api that will be called by the api gateway layer. We can expect that
+	// 
+	documentId, err := ds.documentRepo.CreateDocument(ctx, ownerUserId, documentName, documentDescription)
+	if err != nil {
+		// var domainError DomainError
+		// // &domainError is a reference to an interface, since interfaces are reference like
+		// // types then we do not have to use a pointer to an interface. We do have to pass the
+		// // address of the interface type so that we can modify the pointer to be pointing at
+		// // a different piece of memory
+		// if !errors.As(err, &domainError) {
+		// 	err = RepoImpl("unexpected error encountered when creating document", err)
+		// }
+		// err.(DomainError) syntax does not check all the way down the error chain but instead 
+		// checks the type of the top error. We want to use this syntax because our goal is to wrap
+		// errors in a domain error instead of find the identity of the error
+		if _, ok := err.(DomainError); !ok {
+			err = RepoImpl("unexpected error encountered when creating document", err)
+		}
+	}
+	return documentId, err
+}

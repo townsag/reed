d19f2541ab09f81bab400e21d235bd5dee78bc1d
Andrew Townsend <townsag@gmail.com>
2026-03-19 19:34:31 -0700

    read database connection envs

--- message_proxy/src/config/postgres.rs
+++ message_proxy/src/config/postgres.rs
@@ -1,17 +1,25 @@
-use sqlx::{Pool, Postgres, postgres::{
-    PgConnectOptions, PgPoolOptions
-},
-Error};
+use std::env;
 
+use sqlx::{
+    Pool, Postgres, postgres::{
+        PgConnectOptions, PgPoolOptions
+    },
+    Error
+};
 
 const MAX_CONNECTIONS: u32 = 10;
 
 pub async fn build_postgres_pool() -> Result<Pool<Postgres>, Error> {
+    // read postgres database connection information from env with defaults
+    let host = env::var("POSTGRES_HOST").unwrap_or("localhost".to_string());
+    let username = env::var("POSTGRES_USER").unwrap_or("admin".to_string());
+    let password = env::var("POSTGRES_PASS").unwrap_or("password".to_string());
+
     let options = PgConnectOptions::new()
-        .host("localhost")
+        .host(&host)
         .port(5432)
-        .username("admin")
-        .password("password")
+        .username(&username)
+        .password(&password)
         .ssl_mode(sqlx::postgres::PgSslMode::Disable);
     PgPoolOptions::new().max_connections(MAX_CONNECTIONS).connect_with(options).await
 }
